85c93efb877f3d6ca7729e912727e82f1221fd0f
Cyangen <matteoratti90@gmail.com>
2019-11-20 16:59:04 +0100

    add categories modal to new-deal page

--- src/app/deals/new-deal/new-deal.page.ts
+++ src/app/deals/new-deal/new-deal.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController } from '@ionic/angular';
+import { CategoriesComponent } from '../categories/categories.component';
+
+
 
 @Component({
   selector: 'app-new-deal',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewDealPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController, private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onSelectCategory () {
+    this.modalCtrl.create({
+      component: CategoriesComponent
+    }).then(modalEl => {
+      modalEl.present();
+      return modalEl.onDidDismiss();
+    });
   }
 
 }
