f7cff906c8cb535f33c781c273fc87f6f481686c
Cyangen <matteoratti90@gmail.com>
2019-11-20 16:59:15 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'categories', pathMatch: 'full' },
+  { path: '', redirectTo: 'deals', pathMatch: 'full' },
   { path: 'sign-in', loadChildren: './sign-in/sign-in.module#SignInPageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'categories', loadChildren: './categories/categories.module#CategoriesPageModule' },
   { path: 'deals', loadChildren: './deals/deals.module#DealsPageModule' }
   // { path: 'new-deal', loadChildren: './deals/new-deal/new-deal.module#NewDealPageModule' }
 ];
